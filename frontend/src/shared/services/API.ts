c17f791c5326f334d8964ff3386ffd06fa5e1263
Tanichael <toma92toop@gmail.com>
2024-06-17 13:30:12 +0900

    グローバル変数をスレッドに対応できる形式に変更

--- frontend/src/shared/services/API.ts
+++ frontend/src/shared/services/API.ts
@@ -10,20 +10,28 @@ export const getHello = createAsyncThunk<Hello>('getHello', async () => {
   return await response.json();
 });
 
+//parentIdが-1のポストは親を持たない
 const boardElementData = [
   {
+    id: 0,
     name: "Toma",
     message: "とてもいい掲示板です。",
+    parentId: -1,
   },
   {
+    id: 1,
     name: "Chono",
     message: "そうは思わない",
+    parentId: -1,
   },
   {
+    id: 2,
     name: "Toma",
     message: "なんで？",
+    parentId: -1,
   }
 ]
+
 export const postBoard = createAsyncThunk<BoardElement[], string>('postBoard',async (message) => {
     // await fetch(`${API_ENDPOINT_PATH}/board`, {
     //   method: 'POST',
@@ -32,12 +40,14 @@ export const postBoard = createAsyncThunk<BoardElement[], string>('postBoard',as
     //   },
     //   body: JSON.stringify({ message }),
     // });
+    const boardElementsSize = boardElementData.length;
+    const nextId =  boardElementsSize;
     boardElementData.push({
+      id: nextId,
       name: "Toma",
       message: message,
+      parentId: -1,
     });
     return boardElementData;
   }
 );
-
-
