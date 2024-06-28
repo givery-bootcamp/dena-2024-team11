7c2b80e13908d8cf94d830d7c47934fa9606492d
keigo-yonezawa <keigo.yonezawa@dena.com>
2024-06-28 14:12:46 +0900

    update: userテーブルにiconとemailを追加

--- mysql/sql/insert.sql
+++ mysql/sql/insert.sql
@@ -1,5 +1,11 @@
-INSERT INTO users (name, password) VALUES ('taro', 'password');
-INSERT INTO users (name, password) VALUES ('hanako', 'PASSWORD');
+INSERT INTO users (name, email, password, icon) VALUES ('taro', 'taro@dena.com', 'password', 'https://avatars.githubusercontent.com/u/162857806?v=4');
+INSERT INTO users (name, email, password) VALUES ('hanako', 'hanako@dena.com', 'PASSWORD');
+INSERT INTO users (name, email, password, icon) VALUES ('shunsuke.koike', 'shunsuke.koike@dena.com', 'shunsuke.koike', 'https://avatars.githubusercontent.com/u/162857806?v=4');
+INSERT INTO users (name, email, password, icon) VALUES ('keigo.yonezawa', 'keigo.yonezawa@dena.com', 'keigo.yonezawa', 'https://avatars.githubusercontent.com/u/71923432?v=4');
+INSERT INTO users (name, email, password, icon) VALUES ('toma.tanigawa','toma.tanigawa@dena.com', 'toma.tanigawa', 'https://avatars.githubusercontent.com/u/61912106?v=4');
+INSERT INTO users (name, email, password, icon) VALUES ('yukihiko.tone', 'yukihiko.tone@dena.com', 'yukihiko.tone', 'https://avatars.githubusercontent.com/u/68996392?v=4');
+INSERT INTO users (name, email, password, icon) VALUES ('tomohiro.kawakami', 'tomohiro.kawakami@dena.com', 'tomohiro.kawakami', 'https://avatars.githubusercontent.com/u/61912106?v=4');
+INSERT INTO users (name, email, password, icon) VALUES ('shunsuke.nagano', 'shunsuke.nagano@dena.com', 'shunsuke.nagano', 'https://avatars.githubusercontent.com/u/38309289?v=4');
 
 INSERT INTO posts (user_id, content) VALUES (1, '質問1\n改行');
 INSERT INTO posts (user_id, content) VALUES (1, '質問2\n改行');
