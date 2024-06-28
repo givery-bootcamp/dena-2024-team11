850b977ebecbfd2eebbc534c71a7704e65806e0d
tone8151 <wasshoi0120@gmail.com>
2024-06-28 16:24:01 +0900

    postのスタンプを削除するapiを実装

--- backend/internal/repositories/stamp_repository.go
+++ backend/internal/repositories/stamp_repository.go
@@ -59,20 +59,6 @@ func (r *StampRepository) CreateReplyStamp(replyId int, userId int, name string)
 	return stampedReply.ToEntity(), nil
 }
 
-func (r *StampRepository) removeStamp(stamp interface{}, preloadAssociations []string, idField string, id int, result interface{}) error {
-	if err := r.Conn.Delete(stamp).Error; err != nil {
-		return err
-	}
-	query := r.Conn
-	for _, association := range preloadAssociations {
-		query = query.Preload(association)
-	}
-	if err := query.Where(idField+" = ?", id).First(result).Error; err != nil {
-		return err
-	}
-	return nil
-}
-
 
 func (r *StampRepository) RemovePostStamp(postId int, userId int, name string) (*entities.Post, error) {
 	postStamp := dao.PostStamp{
@@ -80,10 +66,14 @@ func (r *StampRepository) RemovePostStamp(postId int, userId int, name string) (
 		UserId:  userId,
 		Name:    name,
 	}
+    if err := r.Conn.Where("name = ? AND user_id = ? AND post_id = ?", name, userId, postId).Delete(&postStamp).Error; err != nil {
+        return nil, err
+    }
+
 	var stampedPost dao.Post
-	err := r.removeStamp(&postStamp, []string{"User", "Replies", "Stamps.User"}, "id", postId, &stampedPost)
-	if err != nil {
+	if err := r.Conn.Where("id = ?", postId).Preload("User").Preload("Replies").Preload("Stamps.User").First(&stampedPost).Error; err != nil {
 		return nil, err
 	}
 	return stampedPost.ToEntity(), nil
 }
+
